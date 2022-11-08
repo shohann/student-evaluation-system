bb13c0dea881fcaf99dd4dc43befe4aea865e117
shohann <shohanurr490@gmail.com>
2022-11-08 16:41:38 +0600

    initiate server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('working');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {    
+    res.send('Hello');
+});
+
+app.listen(3001, () => {    
+    console.log(`Running Express Server`);
+})
+
