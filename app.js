b4f0cdbc597808817f3004aee5c5350b3adfd182
shohann <shohanurr490@gmail.com>
2022-11-19 11:56:20 +0600

    add options

--- app.js
+++ app.js
@@ -20,21 +20,33 @@ app.post('/quiz', async (req, res) => {
             data: {
                 name: name
             },
-        })
+        });
 
         const question = await prisma.question.create({
             data: {
                 quizId: quiz.id,
                 question_text: req.body.question_text
             },
-        })
+        });
 
+        const options = await prisma.option.createMany({
+            data: req.body.options,
+            skipDuplicates: true, 
+        });
 
-        res.send({ quiz, question })
+        const answer = await prisma.answer.create({
+            data: {
+                answer_text: req.body.answer
+            },
+        });
+
+        res.send({ quiz, question, options, answer })
     } catch(error) {
         console.log(error)
         res.send(error);
-    }
+    };
+
+
 });
 
 app.listen(3001, () => {    
